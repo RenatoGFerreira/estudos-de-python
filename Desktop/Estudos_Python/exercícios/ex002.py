8b2ecd08ebae5170f3ebf074618c98f976035bda
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-16 15:38:58 -0300

    Add ex002.py

--- Desktop/Estudos_Python/exercícios/ex002.py
+++ Desktop/Estudos_Python/exercícios/ex002.py
@@ -1,1 +1,9 @@
 # Faça um programa que leia o nome de uma pessoa e mostre uma msg de boas vindas
+
+nome = input('Digite seu nome: ')
+
+print(f'Olá {nome}, seja muito bem vindo.')
+
+print('Olá',nome,', seja muito bem vindo.')
+
+print('Olá {}, seja muito bem vindo.' .format(nome))
