ddca82afb2e65df8d29deb60ea980ceb499b41d7
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-16 15:57:53 -0300

    Exercícios 3 e 4 add

--- Desktop/Estudos_Python/exercícios/ex003.py
+++ Desktop/Estudos_Python/exercícios/ex003.py
@@ -1,2 +1,8 @@
 '''Crie um programa que leia dois números inteiros e mostre a soma entre eles.'''
 
+numero_1 = int(input('Digite o número 1: '))
+numero_2 = int(input('Digite o número 2: '))
+
+soma = numero_1 + numero_2
+
+print(f'O resultado da soma de {numero_1} e {numero_2} é igual a {soma}.')
--- Desktop/Estudos_Python/exercícios/ex004.py
+++ Desktop/Estudos_Python/exercícios/ex004.py
@@ -1,2 +1,9 @@
-'''Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todos as informaçoes possíeis sobre ele.'''
+'''Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todos as informaçoes possíveis sobre ele.'''
 
+algo = input('Digite algo: ')
+
+print('É número: ', algo.isnumeric())
+print('Está em maiúsculo: ', algo.isupper())
+print('É letra: ', algo.isalpha())
+print('Está em minusculo: ', algo.islower())
+print('Está captalizada: ', algo.istitle())
