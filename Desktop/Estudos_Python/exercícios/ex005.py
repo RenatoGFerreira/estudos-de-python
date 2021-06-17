dcead9ad8d1691333f5a25ff133080fe77e792f1
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-17 18:17:57 -0300

    ex005 add

--- Desktop/Estudos_Python/exercícios/ex005.py
+++ Desktop/Estudos_Python/exercícios/ex005.py
@@ -1,1 +1,5 @@
 #Faca um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor
+
+num = int(input('Digite um número: '))
+
+print(f'O antecessor do número {num} é {num-1} e seu sucessor é {num+1}')
