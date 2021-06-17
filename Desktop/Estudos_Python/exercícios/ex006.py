f9d224ef1e6b31fd51427867c4b740aae938a292
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-17 18:26:02 -0300

    ex006.py add

--- Desktop/Estudos_Python/exercícios/ex006.py
+++ Desktop/Estudos_Python/exercícios/ex006.py
@@ -1,1 +1,5 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+num = int(input(f'Entre com um número: '))
+
+print(f'Para o número {num}, seu dobro é {num*2}, seu triplo é {num*3} e sua raiz quadrada é {num**(1/2)}')
