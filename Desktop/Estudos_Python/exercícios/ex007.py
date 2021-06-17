9b9c9a744184b397b95beaae04ddeab2fe6c1c77
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-17 18:38:56 -0300

    Novos Exercícios add

---
+++ Desktop/Estudos_Python/exercícios/ex007.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+
+num_1 = float(input('Digite a primeira nota: '))
+num_2 = float(input('Digite a segunda nota: '))
+
+print(f'A média das notas {num_1} e {num_2} é igual a {(num_1+num_2)/2}.')
---
+++ Desktop/Estudos_Python/exercícios/ex008.py
@@ -0,0 +1,5 @@
+#Escreva um programa que leia um valor em métros e o exiba convertido em centímetros e milimetros
+
+medida = int(input('Digite o valor em métros: '))
+
+print(f'A medida de {medida} metros é igual a {medida*100} centímetros e {medida*1000} milímetros.')
