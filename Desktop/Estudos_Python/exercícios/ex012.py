092542790c0b2fd3da1fc109c89c15b4ff73502e
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-20 09:38:00 -0300

    Ex012.py

--- Desktop/Estudos_Python/exercícios/ex012.py
+++ Desktop/Estudos_Python/exercícios/ex012.py
@@ -1,1 +1,7 @@
 #Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+price = float(input('Preço do Produto: '))
+
+new_price = price * 0.95
+
+print(f'O produto de R${price:.2f} com o desconto de 5% fica R${new_price:.2f}. ')
