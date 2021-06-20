744d17cac67571e2cde08dc07c92aa8a539f8c11
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-20 09:44:09 -0300

    ex013.py

--- Desktop/Estudos_Python/exercícios/ex013.py
+++ Desktop/Estudos_Python/exercícios/ex013.py
@@ -1,1 +1,6 @@
 #Faça um algoitmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salary = float(input('Salário total: R$'))
+salary_adjust = salary * 1.15
+
+print(f'Para o salário de R${salary:.2f}, o salário com o aumento de 15% passa a ser: R${salary_adjust:.2f}.')
