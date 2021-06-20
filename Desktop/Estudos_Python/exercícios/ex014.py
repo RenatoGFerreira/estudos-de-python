ef80ff5ccf7a3aa371f1ab172d06da13cb1903a4
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-20 09:54:55 -0300

    ex014.py

--- Desktop/Estudos_Python/exercícios/ex014.py
+++ Desktop/Estudos_Python/exercícios/ex014.py
@@ -1,1 +1,7 @@
 #Escreva um programa que converta uma temperatura digitada em ºC E CONVERTA PARA ºF.
+
+
+temperature_celsius = float(input('Enter with temperature Celsius: '))
+temperature_fahrenheit = ((9* temperature_celsius)/5)+32
+
+print(f'A temperatura {temperature_celsius}ºC é o mesmo que {temperature_fahrenheit}ºF.')
