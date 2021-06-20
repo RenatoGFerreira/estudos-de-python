0d5afd4565562bf3676c0433249d985d7dda474a
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-20 09:33:05 -0300

    Ex011.py

--- Desktop/Estudos_Python/exercícios/ex011.py
+++ Desktop/Estudos_Python/exercícios/ex011.py
@@ -1,3 +1,12 @@
 '''
     Faça um programa que leia a largura e a altura de uma parede em metos, calcule a sua àrea e a quantidade de tinta necessária para pintá-la sabendo que cada litro de tinta, pinta uma área de 2m²
 '''
+
+largura = float(input('Largura da Parede: '))
+altura = float(input('Altura da Parede: '))
+
+
+area_total = largura * altura
+
+print(f'Com a àrea da parede de {area_total}m² será necessário {area_total/2} litros de tinta.')
+
