1b6176b843403aac1b8a38013fcbbf18ee443f2b
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-20 10:39:37 -0300

    new ex16-24 add

--- Desktop/Estudos_Python/exercícios/ex016.py
+++ Desktop/Estudos_Python/exercícios/ex016.py
@@ -1,1 +1,5 @@
-#Crie um programa que leia um número Real qualqer pelo teclado e mostre a tela a sua porção inteira
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre a tela a sua porção inteira.
+
+num_real = float(input('Digite um número real: '))
+
+print(f'A porção inteira do número {num_real} é igual a {int(num_real)}.')
--- Desktop/Estudos_Python/exercícios/ex017.py
+++ Desktop/Estudos_Python/exercícios/ex017.py
@@ -1,3 +1,12 @@
 '''
     Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
 '''
+
+import math
+
+cateto_oposto = float(input('Entre com o comprimento do Cateto Oposto: '))
+cateto_adjacente = float(input('Entre com o comprimento do Cateto Adjacente: '))
+
+hipotenusa = math.sqrt(cateto_oposto**2 + cateto_adjacente**2)
+
+print(f'Para o cateto oposto de {cateto_oposto} e cateto adjacente de {cateto_adjacente} temos o comprimento da hipotenusa de {hipotenusa:.2f}')
