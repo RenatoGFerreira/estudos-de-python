23161c70627a351879e02fc2510907e87ea8aba7
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-23 18:24:18 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex024.py
+++ Desktop/Estudos_Python/exercícios/ex024.py
@@ -2,5 +2,9 @@
 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
 '''
 
-cidade = str(input('Nome da cidade: '))
+cidade = str(input('Nome da cidade: ')).strip().lower()
+
+sim_ou_não = 'santo' in cidade[0:5]
+
+print(f'O nome da cidade começa com Santo? [True/False] {sim_ou_não}')
 
