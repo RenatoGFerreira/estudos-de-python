a92b69d33320734a07d149b4cd94f875f7337a48
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-23 21:01:25 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex030.py
+++ Desktop/Estudos_Python/exercícios/ex030.py
@@ -2,3 +2,9 @@
 Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou IMPAR.
 '''
 
+impar_ou_par = int(input('Número: '))
+
+if impar_ou_par % 2 == 1:
+    print('Impar')
+else:
+    print('Par')
