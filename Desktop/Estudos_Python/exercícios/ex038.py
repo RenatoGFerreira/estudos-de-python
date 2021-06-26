214ba1ddcbedaec8af3d3dd5cb34f46022e3d4b0
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-26 11:16:59 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex038.py
+++ Desktop/Estudos_Python/exercícios/ex038.py
@@ -4,3 +4,13 @@ Escreva um programa que leia dois números inteiros e compare-os, mostrando na t
     -O segundo valor é maior.
     -Não existe valor maior, os dois são iguais
 '''
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+
+if numero_1 > numero_2:
+    print('O primeiro número é maior que o segundo.')
+elif numero_1 < numero_2:
+    print('O segundo número é maior que o primeiro.')
+else:
+    print('Não existe valor maior, os dois são iguais.')
