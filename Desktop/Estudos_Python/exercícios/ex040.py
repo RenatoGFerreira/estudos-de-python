52ea07fa85908e974b254eb3793e8160dc758bc8
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-26 11:50:06 -0300

    exercicios do 31 ao 40 add

--- Desktop/Estudos_Python/exercícios/ex040.py
+++ Desktop/Estudos_Python/exercícios/ex040.py
@@ -5,3 +5,15 @@ a média atingida.
     - Média entre 5,0 e 6,9: RECUPERAÇÃO
     - Média 7,0 ou superior: APROVADO
 '''
+
+nota_1 = float(input('Nota 1 do aluno: '))
+nota_2 = float(input('Nota 2 do aluno: '))
+
+media = (nota_2 + nota_1) / 2
+
+if media >= 7:
+    print(f'media: {media:.2f} = APROVADO')
+elif media < 5:
+    print(f'media: {media:.2f} = REPROVADO')
+else:
+    print(f'media: {media:.2f} = RECUPERAÇÃO')
