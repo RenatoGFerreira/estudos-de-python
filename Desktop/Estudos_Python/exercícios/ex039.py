13f676acf3fd5a7b9aed5347270c775c173cc364
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-26 11:34:02 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex039.py
+++ Desktop/Estudos_Python/exercícios/ex039.py
@@ -6,3 +6,17 @@ Faça um program que leia o ano de nascimento de um jovem e informe, de acordo c
 
 Seu programa tambem deverá mostrar o tempo que falta ou passou do prazo.
 '''
+
+from datetime import date
+
+ano_atual = date.today().year
+print(ano_atual)
+
+ano_nascimento = int(input('Qual o seu ano de nascimento? '))
+if ano_atual - ano_nascimento < 18:
+    print(f'Faltam {18 - (ano_atual - ano_nascimento)} anos para o seu alistamento.')
+elif ano_atual - ano_nascimento > 18:
+    print(f'Já se passaram {(ano_atual - ano_nascimento) - 18} anos do seu alistamento.')
+else:
+    print('Com 18 anos está no ano de alistamento.')
+
