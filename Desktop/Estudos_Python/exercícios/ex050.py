747a583d8edc3f01a48e26366a96605677cd6556
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-27 14:07:01 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex050.py
+++ Desktop/Estudos_Python/exercícios/ex050.py
@@ -2,3 +2,11 @@
 Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor
 digitado for impar, desconsidere-o.
 '''
+
+soma = 0
+
+for cont in range(0, 6):
+    numero = int(input(f'Digite o {cont+1}º número: '))
+    if numero % 2 == 0:
+        soma += numero
+print(f'A soma total é {soma}.')
