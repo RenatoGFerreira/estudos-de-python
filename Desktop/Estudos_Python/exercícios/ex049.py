5f17f741a5809ae52abb7f7caaa7e1c0f86bd57e
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-27 13:58:36 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex049.py
+++ Desktop/Estudos_Python/exercícios/ex049.py
@@ -1,3 +1,9 @@
 '''
 Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço FOR.
 '''
+print('=='*20)
+numero = int(input('Escolha um número: '))
+print('=='*20)
+for num in range(0, 11):
+    print(f'{numero} x {num}= {numero*num}')
+print('=='*20)
