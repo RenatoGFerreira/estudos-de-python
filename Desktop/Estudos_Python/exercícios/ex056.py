8cb4e711db18aa5092c9e7c2466ffd4faf1e65e6
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-27 14:19:57 -0300

    exercícios

---
+++ Desktop/Estudos_Python/exercícios/ex056.py
@@ -0,0 +1,6 @@
+'''
+Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa mostre:
+    * A idade média do grupo
+    * Qual é o nome do homem mais velho.
+    * Quantas mulheres tem menos de 20 anos
+'''
---
+++ Desktop/Estudos_Python/exercícios/ex059.py
@@ -0,0 +1,11 @@
+'''
+    Crie um programa que leia dois valores e mostre um menu na tela:
+
+[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos Números
+[5] Sair do Programa
+
+Seu programa deverá realizar a operação solicitada em cada caso.
+'''
