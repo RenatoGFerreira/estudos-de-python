868990f7f6212db5f4a1325944b5ad2d8ef31242
Renato Ferreira <renatoyferreira@gmail.com>
2021-06-27 12:15:06 -0300

    exercícios

--- Desktop/Estudos_Python/exercícios/ex043.py
+++ Desktop/Estudos_Python/exercícios/ex043.py
@@ -7,4 +7,24 @@ tabela abaixo:
     - 25 até 30: Sobrepeso
     - 30 a 40: Obesidade
     - Acima de 40: Obesidade Mórbida
+
 '''
+
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+
+imc = peso / (altura * altura)
+
+if imc < 18.5:
+    print(f'Para imc de {imc:.2f}: Abaixo do Peso')
+elif imc >= 18.5 and imc < 25:
+    print(f'Para imc de {imc:.2f}: Peso Ideal')
+elif imc >= 25 and imc < 30:
+    print(f'Para imc de {imc:.2f}: Sobrepeso')
+elif imc >= 30 and imc < 40:
+    print(f'Para imc de {imc:.2f}: Obesidade')
+else:
+    print(f'Para imc de {imc:.2f}: Obesidade Mórbida')
+
+
+
